1abf09faa4b96745689fe5cfa17093b095b511eb
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2024-11-26 02:42:01 +0800

    feat: add more actions for notifications

--- src/apis/notification.ts
+++ src/apis/notification.ts
@@ -78,10 +78,18 @@ export async function fetchNotifications(
   }
 }
 
-export async function archiveNotification(
+export async function mutateNotification(
+  action:
+    | 'mark'
+    | 'unmark'
+    | 'archive'
+    | 'unarchive'
+    | 'subscribe'
+    | 'unsubscribe'
+    | 'star'
+    | 'unstar',
   id: string | string[],
   authenticityToken: string,
-  unarchive = false,
 ): Promise<void> {
   const formData = new FormData()
   const ids = toArray(id)
@@ -89,7 +97,63 @@ export async function archiveNotification(
     formData.append('notification_ids[]', id)
   }
   formData.append('authenticity_token', authenticityToken)
-  await request.post(`notifications/beta/${unarchive ? 'un' : ''}archive`, {
+  await request.post(`notifications/beta/${action}`, {
     body: formData,
   })
+}
+
+export function archiveNotification(
+  id: string | string[],
+  authenticityToken: string,
+): Promise<void> {
+  return mutateNotification('archive', id, authenticityToken)
+}
+
+export function unarchiveNotification(
+  id: string | string[],
+  authenticityToken: string,
+): Promise<void> {
+  return mutateNotification('unarchive', id, authenticityToken)
+}
+
+export function readNotification(
+  id: string | string[],
+  authenticityToken: string,
+): Promise<void> {
+  return mutateNotification('mark', id, authenticityToken)
+}
+
+export function unreadNotification(
+  id: string | string[],
+  authenticityToken: string,
+): Promise<void> {
+  return mutateNotification('unmark', id, authenticityToken)
+}
+
+export function subscribeNotification(
+  id: string | string[],
+  authenticityToken: string,
+): Promise<void> {
+  return mutateNotification('subscribe', id, authenticityToken)
+}
+
+export function unsubscribeNotification(
+  id: string | string[],
+  authenticityToken: string,
+): Promise<void> {
+  return mutateNotification('unsubscribe', id, authenticityToken)
+}
+
+export function starNotification(
+  id: string | string[],
+  authenticityToken: string,
+): Promise<void> {
+  return mutateNotification('star', id, authenticityToken)
+}
+
+export function unstarNotification(
+  id: string | string[],
+  authenticityToken: string,
+): Promise<void> {
+  return mutateNotification('unstar', id, authenticityToken)
 }
